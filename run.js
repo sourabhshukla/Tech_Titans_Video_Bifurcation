9d9b3d5576fe382e0dfca66d9dac99a1d13c6296
unknown <sourabhshukla9893502147@gmail.com>
2023-09-05 19:03:21 +0530

    Integrated getTimeStamps to run.js

--- run.js
+++ run.js
@@ -1,17 +1,24 @@
 const generateFrames = require("./generateFrames");
 const getNumberOfFrames = require("./getNumberOfFrames");
 const generateTextFilesForFrames = require("./generateTextFilesForFrames");
-const readTextFile = require("./readTextFile");
+const getTimeStamps = require("./getTimeStamps.js");
+const writeToNodeFile = require("./writeToNodeFile");
 
 const run = async () => {
-  const inputVideoPath = "input.mp4";
-  const outputFramesDir = "frames/";
-  const outputFPS = 1;
-  const startFrame = 1;
-  const headingsPath = "headings.txt";
-  await generateFrames(inputVideoPath, outputFramesDir, outputFPS);
-  const totalFrameNumbers = getNumberOfFrames();
-  await generateTextFilesForFrames(startFrame, totalFrameNumbers);
+  try {
+    const inputVideoPath = "input.mp4";
+    const outputFramesDir = "frames/";
+    const outputFPS = 1;
+    const startFrame = 1;
+    const headingsPath = "headings.txt";
+    await generateFrames(inputVideoPath, outputFramesDir, outputFPS);
+    const totalFrameNumbers = getNumberOfFrames();
+    await generateTextFilesForFrames(startFrame, totalFrameNumbers);
+    const data = await getTimeStamps(headingsPath, totalFrameNumbers);
+    await writeToNodeFile("timestamps.txt", data);
+  } catch (err) {
+    console.log(`Error: ${err}`);
+  }
 };
 
 run();
