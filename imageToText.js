0fddfd85b820f94abe1bef94e6ec6da50855150c
unknown <sourabhshukla9893502147@gmail.com>
2023-09-05 17:29:33 +0530

    Added utility to convert image to text using tesseract.js

---
+++ imageToText.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+const Tesseract = require("tesseract.js");
+
+const imageToText = async (inputImagePath) => {
+  //const inputImagePath = "./frames/frame-796.png";
+
+  const imageBuffer = await fs.readFileSync(inputImagePath);
+
+  let data;
+  await Tesseract.recognize(imageBuffer, "eng")
+    .then(async ({ data: { text } }) => {
+      data = text;
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+  console.log(data);
+  return data;
+};
+
+module.exports = imageToText;
